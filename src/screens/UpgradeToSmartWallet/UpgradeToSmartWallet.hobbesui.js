665962430f4b247e5002643308af8f5594284e45
diablo <jaime@diablourbano.com>
2019-04-26 11:55:15 -0300

    brings Me, Persona, PersonaWorkflow and UpgradeToSmartWallet UI implementation to update hobbesui state

---
+++ src/screens/UpgradeToSmartWallet/UpgradeToSmartWallet.hobbesui.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Alert } from 'react-native';
+import { Hobbes } from 'hobbesui';
+
+import Scene from './scene';
+
+Hobbes.add({
+  parent: 'SCREEN',
+  group: 'SmartContract',
+  id: 'UPGRADE_SMART_CONTRACT',
+  title: 'Upgrade to SmartContract',
+  component: (
+    <Scene
+      onBack={() => Alert.alert('back to assets')}
+      onUpgrade={() => Alert.alert('upgrade user to SmartWallet')}
+    />
+  ),
+});
